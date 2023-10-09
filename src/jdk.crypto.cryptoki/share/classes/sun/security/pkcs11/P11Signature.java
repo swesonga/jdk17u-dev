4d284c1b2bfacc4cad7036c5687e14c833fd3bd7
Saint Wesonga <saint.wesonga@microsoft.com>
2023-10-09 11:09:51 -0600

    Backport 334b977259930368160db705c1f2feda0b0e8707

--- src/jdk.crypto.cryptoki/share/classes/sun/security/pkcs11/P11Signature.java
+++ src/jdk.crypto.cryptoki/share/classes/sun/security/pkcs11/P11Signature.java
@@ -752,9 +752,12 @@ final class P11Signature extends SignatureSpi {
             int len = (p11Key.length() + 7) >> 3;
             RSAPadding padding = RSAPadding.getInstance
                                         (RSAPadding.PAD_BLOCKTYPE_1, len);
-            byte[] padded = padding.pad(data);
-            return padded;
-        } catch (GeneralSecurityException e) {
+            byte[] result = padding.pad(data);
+            if (result == null) {
+                throw new ProviderException("Error padding data");
+            }
+            return result;
+        } catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
             throw new ProviderException(e);
         }
     }
